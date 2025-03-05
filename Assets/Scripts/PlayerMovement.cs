43b906493c1c7d19b0a91a38c229e93792129457
HogRidee <iaraoz061102@gmail.com>
2025-03-05 12:48:31 -0500

    Update script to control walk and idle animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,31 @@ public class PlayerMovement : MonoBehaviour
     private float m_moveSpeed = 5f;
     private Rigidbody2D m_rb;
     private Vector2 m_moveInput;
-    
-    public void Move(InputAction.CallbackContext context)
-    {
-        m_moveInput = context.ReadValue<Vector2>();
-    }
+    private Animator m_animator;
     
     private void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         m_rb.linearVelocity = m_moveInput * m_moveSpeed;
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        m_animator.SetBool("isWalking", true);
+        if (context.canceled)
+        {
+            m_animator.SetBool("isWalking", false);
+            m_animator.SetFloat("LastInputX", m_moveInput.x);
+            m_animator.SetFloat("LastInputY", m_moveInput.y);
+        }
+        m_moveInput = context.ReadValue<Vector2>();
+        m_animator.SetFloat("InputX", m_moveInput.x);
+        m_animator.SetFloat("InputY", m_moveInput.y);
+    }
+
 }
